44e434d4dd0f6ced235e16be1ed4fa09137914ae
Austin Pocus <ajpocus@gmail.com>
2021-12-05 16:25:45 -0500

    perfik

--- main.lua
+++ main.lua
@@ -1,7 +1,14 @@
 local cartographer = require "lib/cartographer"
+local logger = require("lib/logging/file") {
+  filename = "~/log.txt"
+}
 
 WIDTH = 1024
 HEIGHT = 768
+TILE = 32
+
+Keys = {}
+MoveQueue = {}
 
 local function isDirection(key)
   return key == "up" or key == "down" or key == "left" or key == "right"
@@ -9,9 +16,9 @@ end
 
 local function getDirection(key)
   if key == "up" then
-    return 0, 1
-  elseif key == "down" then
     return 0, -1
+  elseif key == "down" then
+    return 0, 1
   elseif key == "left" then
     return -1, 0
   elseif key == "right" then
@@ -21,9 +28,30 @@ local function getDirection(key)
   end
 end
 
+local function isMovable(dx, dy)
+  local nx = Player.x + dx
+  local ny = Player.y + dy
+
+  return nx >= 0 and nx <= WIDTH and ny >= 0 and ny <= HEIGHT
+end
+
+local function isMoving()
+  return Player.dx ~= 0 or Player.dy ~= 0
+end
+
+local function tlen(t)
+  local count = 0
+
+  for _ in pairs(t) do
+    count = count + 1
+  end
+
+  return count
+end
+
 Player = {
-  x = 0,
-  y = 0,
+  x = 1,
+  y = 1,
   dx = 0,
   dy = 0
 }
@@ -40,8 +68,8 @@ function love.load()
 end
 
 function love.update(dt)
-  Player.x = Player.x + Player.dx * dt
-  Player.y = Player.y + Player.dy * dt
+  Player.x = Player.x + Player.dx * TILE * dt
+  Player.y = Player.y + Player.dy * TILE * dt
 end
 
 function love.draw()
@@ -50,19 +78,44 @@ function love.draw()
 end
 
 function love.keypressed(key, scancode, isrepeat)
+  if isrepeat then return end
+
+  Keys[key] = true
+  print("#IN", tlen(Keys))
   if key == "escape" then
      love.event.quit()
   elseif isDirection(key) then
     local dx, dy = getDirection(key)
+    if not isMovable(dx, dy) or isMoving() then
+      print("OPE")
+      table.insert(MoveQueue, { dx, dy })
+      return
+    end
+
     local nx = Player.x + dx
     local ny = Player.y + dy
     local layer = Map.layers[1]
-    local gid = layer.getTileAtPixelPosition(nx, ny)
-    local issolid = Map.getTileProperty(gid, "solid")
+    local gid = layer:getTileAtPixelPosition(nx, ny)
+    local issolid = Map:getTileProperty(gid, "solid")
 
-    if not issolid then
-      Player.dx = dx
-      Player.dy = dy
-    end
+    if issolid then return end
+  
+    Player.dx = dx
+    Player.dy = dy
+  end
+end
+
+function love.keyreleased(key, scancode)
+  Keys[key] = nil
+  print("#" .. tlen(Keys))
+  if isDirection(key) and tlen(MoveQueue) > 0 then
+    local queuedMove = MoveQueue[1]
+    table.remove(MoveQueue, 1)
+    local dx, dy = unpack(queuedMove)
+    Player.dx = dx
+    Player.dy = dy
+  else
+    Player.dx = 0
+    Player.dy = 0
   end
 end
