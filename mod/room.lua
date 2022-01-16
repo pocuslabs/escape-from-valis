c467b1abf165176fa8efa6786b201979ba4eb6cf
Austin Pocus <ajpocus@gmail.com>
2022-01-16 18:15:09 -0500

    major overhaul of the mapping system in progress

--- mod/room.lua
+++ mod/room.lua
@@ -11,7 +11,7 @@ function Room:new(x, y, w, h)
   y = math.abs(y) or 1
   w = w or 1
   h = h or 1
-  self.posX, self.posY = help.tileToPixel(x, y)
+  self.x, self.y = help.tileToPixel(x, y)
   self.w, self.h = help.tileToPixel(w, h)
   self.map = {}
 
@@ -34,8 +34,8 @@ function Room:new(x, y, w, h)
 end
 
 function Room:isInside(screenX, screenY)
-  local xWithin = screenX >= self.posX and screenX <= (self.posX + self.w)
-  local yWithin = screenY >= self.posY and screenY <= (self.posY + self.h)
+  local xWithin = screenX >= self.x and screenX <= (self.x + self.w)
+  local yWithin = screenY >= self.y and screenY <= (self.y + self.h)
   return xWithin and yWithin
 end
 
